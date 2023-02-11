2634dc9eeb2fd580c1b0b2528ba63321f5aba2c4
Ananya291 <ananyat9999@gmail.com>
2023-02-11 20:55:05 +0530

    first commit

---
+++ src/Banner.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+//Fat arrow operator
+const Banner = () => {
+  return (
+    <div>
+      {/* //Text */}
+      <div>
+        <h1>I'm Designer</h1>
+      </div>
+      <div>
+        <img src="" />
+      </div>
+    </div>
+  );
+};
+export default Banner;
---
+++ src/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div>
+      <img></img>
+      <h1>About</h1>
+      <h1>Contact us</h1>
+      <h1>Pricing</h1>
+    </div>
+  );
+}
+
+export default NavBar;
